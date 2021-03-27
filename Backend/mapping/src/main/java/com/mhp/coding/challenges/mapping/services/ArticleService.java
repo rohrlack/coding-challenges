534274ba8bec22ef9308b68afb4d7801e91f1cd5
rohrlack <>
2021-03-27 21:48:42 +0100

    use ArticleMapper in ArticleService

--- Backend/mapping/src/main/java/com/mhp/coding/challenges/mapping/services/ArticleService.java
+++ Backend/mapping/src/main/java/com/mhp/coding/challenges/mapping/services/ArticleService.java
@@ -4,40 +4,36 @@ import com.mhp.coding.challenges.mapping.mappers.ArticleMapper;
 import com.mhp.coding.challenges.mapping.models.db.Article;
 import com.mhp.coding.challenges.mapping.models.dto.ArticleDto;
 import com.mhp.coding.challenges.mapping.repositories.ArticleRepository;
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Service
+@RequiredArgsConstructor
 public class ArticleService {
 
     private final ArticleRepository repository;
 
     private final ArticleMapper mapper;
 
-    @Autowired
-    public ArticleService(ArticleRepository repository, ArticleMapper mapper) {
-        this.repository = repository;
-        this.mapper = mapper;
-    }
-
     public List<ArticleDto> list() {
         final List<Article> articles = repository.all();
-        //TODO
-        return new ArrayList<>();
+        return articles.stream()
+                .map(mapper::articleToArticleDto)
+                .collect(Collectors.toList());
     }
 
-    public ArticleDto articleForId(Long id) {
-        final Article article = repository.findBy(id);
-        //TODO
-        return new ArticleDto();
+    public Optional<ArticleDto> articleForId(Long id) {
+        final Optional<Article> article = repository.findBy(id);
+        return article.map(mapper::articleToArticleDto);
     }
 
     public ArticleDto create(ArticleDto articleDto) {
         final Article create = mapper.map(articleDto);
         repository.create(create);
-        return mapper.map(create);
+        return mapper.articleToArticleDto(create);
     }
 }
