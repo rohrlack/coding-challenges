a51c7bf857bfe9233ccec93a0dee7ea9677f155c
rohrlack <>
2021-03-27 21:45:25 +0100

    publish inquiry as event in InquiryService

--- Backend/dependency/inquiry/src/main/java/com/mhp/coding/challenges/dependency/inquiry/InquiryService.java
+++ Backend/dependency/inquiry/src/main/java/com/mhp/coding/challenges/dependency/inquiry/InquiryService.java
@@ -1,16 +1,22 @@
 package com.mhp.coding.challenges.dependency.inquiry;
 
+import lombok.RequiredArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.stereotype.Component;
 
 @Component
+@RequiredArgsConstructor
 public class InquiryService {
 
     private static final Logger LOG = LoggerFactory.getLogger(InquiryService.class);
 
+    private final ApplicationEventPublisher applicationEventPublisher;
+
     public void create(final Inquiry inquiry) {
         LOG.info("User sent inquiry: {}", inquiry);
+        applicationEventPublisher.publishEvent(inquiry);
     }
 
 }
